b7fd22aa211d2cc26205626daa3fb859516f1a32
alex-ge123 <1285885272@qq.com>
2020-10-10 16:28:53 +0800

    我的课程

--- src/components/views/myclass.vue
+++ src/components/views/myclass.vue
@@ -1,11 +1,26 @@
 <template>
-   <van-nav-bar
-  title="我的课程"
-  left-text="返回"
+<div1>
+   <van-nav-bar title="我的课程" left-text="返回" left-arrow@click-left="onClickLeft"/>
+<div2>
+  <van-card
+  num="2"
+  price="2.00"
+  desc="描述信息"
+  title="商品标题"
+  thumb="https://img.yzcdn.cn/vant/ipad.jpeg"
+>
+  <template #tags>
+    <van-tag plain type="danger">标签</van-tag>
+    <van-tag plain type="danger">标签</van-tag>
+  </template>
+  <template #footer>
+    <van-button size="mini">按钮</van-button>
+    <van-button size="mini">按钮</van-button>
+  </template>
+</van-card>
+</div2>
 
-  left-arrow
-  @click-left="onClickLeft"/>
-
+</div1>
 </template>
 <script>
 export default {
