071114c57ca9384479d47d5706915cdc5ecef201
Army <1506309724@qq.com>
2020-10-15 15:39:05 +0800

    Army commit

--- src/main.js
+++ src/main.js
@@ -8,6 +8,15 @@ import 'vant/lib/index.css';
 Vue.use(Vant);
 Vue.config.productionTip = false
 
+//Vue.use(VueAxios, axios)
+
+// 引用axios，并设置基础URL为后端服务api地址
+var axios = require('axios')
+axios.defaults.baseURL = 'http://localhost:8088/api'  //此处是https协议如果不是改成http
+
+// 将API方法绑定到全局
+Vue.prototype.$axios = axios
+
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
